11f987ed20cba4fd7edc43c93d1cd1421b71ffe1
Thomas Getgood <thomas.getgood@gmail.com>
2019-02-01 18:44:21 -0500

    How to store lambdas in a database?

--- src/editor/core.clj
+++ src/editor/core.clj
@@ -1,59 +1,14 @@
 (ns editor.core
-  (:require [datomic.api :as d]))
+  (:require [datomic.api :as d]
+            [clojure.datafy :refer [datafy]]
+            [clojure.walk :as walk]))
 
 (def db-uri "datomic:mem://test" #_"datomic:free://localhost:4334/dummy")
 
 (def schema
-  [{:db/ident :version/tag
-    :db/valueType :db.type/keyword
-    :db/cardinality :db.cardinality/one}
+  [
 
-   {:db/ident :version/topology
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/one}
-
-   {:db/ident :version/namespace
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/many}
-
-   {:db/ident :namespace/name
-    :db/valueType :db.type/keyword
-    ;; Should this be unique?
-    :db/cardinality :db.cardinality/one}
-
-   {:db/ident :namespace/binding
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/many}
-
-   {:db/ident :namespace.binding/name
-    :db/valueType :db.type/keyword
-    :db/cardinality :db.cardinality/many}
-
-   {:db/ident :namespace.binding/form
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/one}
-
-   ;; forms
-
-   {:db/ident :fn/string
-    :db/doc "Only fns and values can be named at present."
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one}
-
-   {:db/ident :fn/valid?
-    :db/doc "Is this fn a valid sexp (reads)?"
-    :db/valueType :db.type/boolean
-    :db/cardinality :db.cardinality/one}
-
-   {:db/ident :value/string
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one}
-
-   {:db/ident :map
-    :db/valueType :db.type/double
-    :db/cardinality :db.cardinality/one}
-
-   ;; types
+   ;; Clojure data types (partial list))
 
    {:db/ident :form/type
     :db/valueType :db.type/ref
@@ -137,7 +92,110 @@
    {:db/ident :map.element/value
     :db/isComponent true
     :db/cardinality :db.cardinality/one
-    :db/valueType :db.type/ref}])
+    :db/valueType :db.type/ref}
+
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;; Functions
+   ;;
+   ;; Functions are special. There's a fundamental difference between code and
+   ;; data, or between code intended to be interpreted by the clojure compiler
+   ;; and what is generally referred to as data in the clojure community.
+   ;;
+   ;; The difference is that code is intended to *do* something whereas data is
+   ;; intended to *express* something. Think of it as operational versus
+   ;; declarative.
+   ;;
+   ;; is (fn [x] x) the same as (fn [y] y)? That depends on how you view the
+   ;; "data". Interpreted as data they are different, but interpreted as lambdas
+   ;; they are alpha-equivalent, which means for all operational intents and
+   ;; purposes they are the same. Subtle. Fascinating?
+   ;;
+   ;; For our present purposes I think it suffices to define a function to be
+   ;; data that satisfies certain criteria. To wit:
+   ;;
+   ;; a function is a list whose first element is the symbol 'fn and whose
+   ;; second is a vector. Furthermore any symbols in the function must be either
+   ;; bound locals, or part of the environment. Here's where it gets slightly
+   ;; weird though: the environment is not a reified thing in the typical
+   ;; sense. There's no global mutable variable like a namespace of a typical
+   ;; clojure file. Instead, the environment is specific to a single function
+   ;; and maps the symbol in the function to a namespace qualified symbol as
+   ;; well as an entity id. The entity id refers to the exact code defining the
+   ;; data or function referred to, whereas the namespace reference allows you
+   ;; to know when you're pointing at something stale. I'm not sure this is the
+   ;; right way to do it. We don't want to overwrite namespaces after all; we
+   ;; want to update them as immutable structures.
+   ;;
+   ;; Maybe a better way to think about it would be to have a symbol point at a
+   ;; namespace and a name within a version. Versions being linked into chains
+   ;; like branches in git so that if we want to compare what a symbol points at
+   ;; now to what it would point at at the head of "master" then we can. Now we
+   ;; need tooling to quickly update references like that. Or do we? Do we want
+   ;; updates like that to be easy, or do we want to encourage people to stick
+   ;; with code that works and not modify it unless there's real value to be
+   ;; had? Churn is a major problem which is only getting worse.
+   ;;
+   ;; Macros present some challenges that I haven't even begun to explore.
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+   {:db/ident :function/form
+    :db/doc "Source sexp of fn"
+    :db/isComponent true
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one}
+
+   {:db/ident :function/environment
+    :db/isComponent true
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one}
+
+   {:db/ident :function.environment/binding
+    :db/isComponent true
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/many}
+
+   {:db/ident :function.environment.binding/symbol
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one}
+
+   {:db/ident :function.environment.binding/reference
+    :db/doc "Namespace binding to which this symbol refers"
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one}
+
+   ;; Namespaces
+
+   {:db/ident :version/tag
+    :db/valueType :db.type/keyword
+    :db/cardinality :db.cardinality/one}
+
+   {:db/ident :version/topology
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one}
+
+   {:db/ident :version/namespace
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/many}
+
+   {:db/ident :namespace/name
+    :db/valueType :db.type/keyword
+    ;; Should this be unique?
+    :db/cardinality :db.cardinality/one}
+
+   {:db/ident :namespace/binding
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/many}
+
+   {:db/ident :namespace.binding/name
+    :db/valueType :db.type/keyword
+    :db/cardinality :db.cardinality/many}
+
+   {:db/ident :namespace.binding/form
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one}])
+
+(defn tempid [v]
+  (if-let [tag (:nofiles/tag (meta v))] tag (d/tempid :db.part/user)))
 
 (defprotocol Datomify
   (tx [this]))
@@ -145,43 +203,43 @@
 (extend-protocol Datomify
   java.lang.Long
   (tx [v]
-    {:db/id (d/tempid :db.part/user)
+    {:db/id (tempid v)
      :form/type :type.number/long
      :long/value v})
 
   java.lang.Double
   (tx [v]
-    {:db/id (d/tempid :db.part/user)
+    {:db/id (tempid v)
      :form/type :type.number/double
      :double/value v})
 
   java.lang.String
   (tx [v]
-    {:db/id (d/tempid :db.part/user)
+    {:db/id (tempid v)
      :form/type :type/string
      :string/value v})
 
   clojure.lang.Symbol
   (tx [v]
-    {:db/id (d/tempid :db.part/user)
+    {:db/id (tempid v)
      :form/type :type/symbol
      :symbol/value (str v)})
 
   clojure.lang.Keyword
   (tx [v]
-    {:db/id (d/tempid :db.part/user)
+    {:db/id (tempid v)
      :form/type :type/keyword
      :keyword/value v})
 
   clojure.lang.PersistentHashSet
   (tx [v]
-    {:db/id (d/tempid :db.part/user)
+    {:db/id (tempid v)
      :form/type :type/set
      :set/element (map tx v)})
 
   clojure.lang.PersistentVector
   (tx [v]
-    {:db/id (d/tempid :db.part/user)
+    {:db/id (tempid v)
      :form/type :type/vector
      :vector/element (map-indexed (fn [i x]
                                     {:db/id (d/tempid :db.part/user)
@@ -191,7 +249,7 @@
 
   clojure.lang.PersistentHashMap
   (tx [v]
-    {:db/id (d/tempid :db.part/user)
+    {:db/id (tempid v)
      :form/type :type/map
      :map/element (map (fn [[k v]]
                          {:db/id (d/tempid :db.part/user)
@@ -202,19 +260,17 @@
   ;; FIXME: Identical implementations... use extend
   clojure.lang.PersistentArrayMap
   (tx [v]
-    (println (meta v))
-    (let [tid (if (:top (meta v)) "top-level" (d/tempid :db.part/user))]
-      {:db/id tid
-       :form/type :type/map
-       :map/element (map (fn [[k v]]
-                           {:db/id (d/tempid :db.part/user)
-                            :map.element/key (tx k)
-                            :map.element/value (tx v)})
-                         v)}))
+    {:db/id (tempid v)
+     :form/type :type/map
+     :map/element (map (fn [[k v]]
+                         {:db/id (d/tempid :db.part/user)
+                          :map.element/key (tx k)
+                          :map.element/value (tx v)})
+                       v)})
 
   clojure.lang.PersistentList
-  (tx [[h & t]]
-    (merge {:db/id (d/tempid :db.part/user)
+  (tx [[h & t :as w]]
+    (merge {:db/id (tempid w)
             :form/type :type/list
             :list/head (tx h)}
            (when t
@@ -261,13 +317,31 @@
                   [(clojurise key) (clojurise value)]))
         element))
 
+(defn init-schema [uri]
+  (d/create-database uri)
+  (d/transact (d/connect uri) schema))
+
+;;;;; Read/Write forms to datomic
+
 (defn save-to-datomic! [conn data]
-  ;; TODO: return id of new entity, not entire tx log
-  (get (:tempids @(d/transact conn [(tx (with-meta data {:top true}))])) "top-level"))
+  (let [tag (str (gensym))]
+    (get (:tempids @(d/transact conn [(tx (with-meta data {:nofiles/tag tag}))])) tag)))
 
 (defn pull-from-datomic! [db eid]
   (clojurise (d/pull db '[*] eid)))
 
-(defn init-schema [uri]
-  (d/create-database uri)
-  (d/transact (d/connect uri) schema))
+;;;;; Dealing with execution
+
+(defn builtin? [x]
+  (contains? (:publics (datafy (the-ns 'clojure.core))) x))
+
+(defn write-fn! [conn form]
+  (get-in @(d/transact conn [{:db/id "the-fn"
+                          :function/form (tx form)}])
+          [:tempids "the-fn"]))
+
+(defn load-fn! [db fid]
+  (let [eid (d/q [:find '?e '. :where [fid :function/form '?e]] db)
+        form (pull-from-datomic! db eid)]
+    (with-meta (eval form) {:source-form form
+                            :db/id fid})))
