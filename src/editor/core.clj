946c5c253463cbf2695befc380011dec655b6023
Thomas Getgood <thomas.getgood@gmail.com>
2019-02-01 16:14:04 -0500

    Data schema.

--- src/editor/core.clj
+++ src/editor/core.clj
@@ -1,7 +1,7 @@
 (ns editor.core
   (:require [datomic.api :as d]))
 
-(def db-uri "datomic:free://localhost:4334/dummy")
+(def db-uri "datomic:mem://test" #_"datomic:free://localhost:4334/dummy")
 
 (def schema
   [{:db/ident :version/tag
@@ -49,9 +49,185 @@
     :db/valueType :db.type/string
     :db/cardinality :db.cardinality/one}
 
+   {:db/ident :map
+    :db/valueType :db.type/double
+    :db/cardinality :db.cardinality/one}
+
+   ;; types
+
+   {:db/ident :form/type
+    :db/valueType :db.type/ref
+    :db/isComponent true
+    :db/cardinality :db.cardinality/one
+    :db/doc "Clojure data type as opposed to datomic schema type."}
+
+   {:db/ident :type.number/long}
+   {:db/ident :type.number/double}
+   {:db/ident :type/string}
+   {:db/ident :type/keyword}
+   {:db/ident :type/symbol}
+   {:db/ident :type/list}
+   {:db/ident :type/vector}
+   {:db/ident :type/set}
+   {:db/ident :type/map}
+
+   ;; Atomic values
+
+   {:db/ident :long/value
+    :db/cardinality :db.cardinality/one
+    :db/valueType :db.type/long}
+
+   {:db/ident :double/value
+    :db/cardinality :db.cardinality/one
+    :db/valueType :db.type/double}
+
+   {:db/ident :keyword/value
+    :db/cardinality :db.cardinality/one
+    :db/valueType :db.type/keyword}
+
+   {:db/ident :string/value
+    :db/cardinality :db.cardinality/one
+    :db/valueType :db.type/string}
+
+   {:db/ident :symbol/value
+    :db/cardinality :db.cardinality/one
+    :db/valueType :db.type/string}
+
+   ;; Collections
+
+   {:db/ident :set/element
+    :db/isComponent true
+    :db/cardinality :db.cardinality/many
+    :db/valueType :db.type/ref}
+
+   {:db/ident :list/head
+    :db/isComponent true
+    :db/cardinality :db.cardinality/one
+    :db/valueType :db.type/ref}
+
+   {:db/ident :list/tail
+    :db/isComponent true
+    :db/cardinality :db.cardinality/one
+    :db/valueType :db.type/ref}
+
+   {:db/ident :vector/element
+    :db/isComponent true
+    :db/cardinality :db.cardinality/many
+    :db/valueType :db.type/ref}
+
+   {:db/ident :vector.element/index
+    :db/cardinality :db.cardinality/one
+    :db/valueType :db.type/long}
+
+   {:db/ident :vector.element/value
+    :db/isComponent true
+    :db/cardinality :db.cardinality/one
+    :db/valueType :db.type/ref}
+
+   {:db/ident :map/element
+    :db/isComponent true
+    :db/cardinality :db.cardinality/many
+    :db/valueType :db.type/ref}
+
+   {:db/ident :map.element/key
+    :db/isComponent true
+    :db/cardinality :db.cardinality/one
+    :db/valueType :db.type/ref}
+
+   {:db/ident :map.element/value
+    :db/isComponent true
+    :db/cardinality :db.cardinality/one
+    :db/valueType :db.type/ref}
+
 
    ]
   )
 
+(defprotocol Datomify
+  (tx [this]))
+
+(extend-protocol Datomify
+  java.lang.Long
+  (tx [v]
+    {:db/id (d/tempid :db.part/user)
+     :form/type :type.number/long
+     :long/value v})
+
+  java.lang.Double
+  (tx [v]
+    {:db/id (d/tempid :db.part/user)
+     :form/type :type.number/double
+     :double/value v})
+
+  java.lang.String
+  (tx [v]
+    {:db/id (d/tempid :db.part/user)
+     :form/type :type/string
+     :string/value v})
+
+  clojure.lang.Symbol
+  (tx [v]
+    {:db/id (d/tempid :db.part/user)
+     :form/type :type/symbol
+     :symbol/value (str v)})
+
+  clojure.lang.Keyword
+  (tx [v]
+    {:db/id (d/tempid :db.part/user)
+     :form/type :type/keyword
+     :keyword/value v})
+
+  clojure.lang.PersistentHashSet
+  (tx [v]
+    {:db/id (d/tempid :db.part/user)
+     :form/type :type/set
+     :set/element (map tx v)})
+
+  clojure.lang.PersistentList
+  (tx [[h & t]]
+    (merge {:db/id (d/tempid :db.part/user)
+            :form/type :type/list
+            :list/head (tx h)}
+           (when t
+             {:list/tail (tx t)}))))
+
+(defmulti clojurise (fn [x] (-> x :form/type :db/ident)))
+
+(defmethod clojurise :type.number/long
+  [{:keys [:long/value]}]
+  value)
+
+(defmethod clojurise :type.number/double
+  [{:keys [:double/value]}]
+  value)
+
+(defmethod clojurise :type/string
+  [{:keys [:string/value]}]
+  value)
+
+(defmethod clojurise :type/keyword
+  [{:keys [:keyword/value]}]
+  value)
+
+(defmethod clojurise :type/symbol
+  [{:keys [:symbol/value]}]
+  (symbol value))
+
+(defmethod clojurise :type/set
+  [{:keys [:set/element]}]
+  (into #{} (map clojurise element)))
+
+(defmethod clojurise :type/list
+  [{:keys [:list/head :list/tail] :as x}]
+  (cons (clojurise head) (when tail (clojurise tail))))
+
+(defn save-to-datomic! [conn data]
+  ;; TODO: return id of new entity, not entire tx log
+  (d/transact conn [(tx data)]))
+
+(defn pull-from-datomic! [db eid]
+  (clojurise (d/pull db '[*] eid)))
+
 (defn init-schema [uri]
+  (d/create-database uri)
   (d/transact (d/connect uri) schema))
