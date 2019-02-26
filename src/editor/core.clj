43e97feee2b6267aa97ff9c7ef275d88d56976bf
Thomas Getgood <thomas.getgood@gmail.com>
2019-02-25 22:44:25 -0500

    Don't need that queue anymore.

--- src/editor/core.clj
+++ src/editor/core.clj
@@ -19,37 +19,11 @@
 (defonce host
   (falloleen.hosts/default-host {:size [1000 1000]}))
 
-
 (defonce p
   @(fx/code-stage))
 
-(defonce q
-  (let [ch (async/chan (async/sliding-buffer 100))]
-    (async/go-loop []
-      (when-let [message (async/<! ch)]
-        (when (= :key-stroke (:type message))
-          (let [text (:text message)]
-            (try
-              (binding [*read-eval* false]
-                (let [form (read-string text)]
-                  (swap! system assoc-in [:namespaces :dumpalump.core]
-                         {:base-render form :*base-render* nil})))
-              (catch Exception e
-                (swap! system assoc-in [:namespaces :dumpalump.core
-                                        :*base-render*]
-                       text)))))
-        (recur)))
-    ch))
-
 (defonce ev-map
   (events/bind-text-area! (:area p)))
-
-(defn create-code-window [sym]
-  (let [ns (keyword (namespace sym))
-        n (keyword (name sym))
-        form (get-in @system [:namespaces ns n])
-        text (with-out-str (pprint form))]
-    ))
 
 #_(def example-xform
   {:key-down (fn [previous event]
